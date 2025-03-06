969b73863db3d526d53b149855f087b4a9f52ea9
Syakib-Arsalan74 <Syakibgithub@gmail.com>
2025-03-06 10:34:17 +0700

    chart.js

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,6 +2,33 @@
     <x-navbar></x-navbar>
     <x-sidebar></x-sidebar>
     <x-content>
-
+        <div>
+            <canvas id="myChart"></canvas>
+        </div>
     </x-content>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+
+        const ctx = document.getElementById('myChart');
+
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: 'Activity',
+                    data: [12, 19, 3, 5, 2, 3],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    </script>
 </x-layout>
